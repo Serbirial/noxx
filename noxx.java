eb799065d3d37837346eb1c323581f5a06c36f10
simran Singh <simransingh9503@gmail.com>
2019-04-23 16:55:09 +1200

    added noxx java syntax

---
+++ noxx.java
@@ -0,0 +1,17 @@
+public class Noxx
+{
+  public static void main(String[] args)
+  {
+    String noxx;
+    noxx = "Gay";
+    
+    if (noxx == "Gay") {
+      System.out.println("Noxx is gay");
+    }
+    
+    else {
+     System.out.println("Noxx is still gay");
+    }
+  }   
+}
+
